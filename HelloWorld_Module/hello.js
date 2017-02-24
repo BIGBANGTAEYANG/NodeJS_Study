a29449da81280d70ce4a7b2c6d3dc8676cbacf62
BIGBANGTAEYANG <2087396991@qq.com>
2017-02-24 12:53:08 +0800

    hellomodule

---
+++ HelloWorld_Module/hello.js
@@ -0,0 +1,8 @@
+exports.world = function() {
+  console.log('Hello World');
+}
+
+//module.exports
+// module.exports = function(){
+//     console.log('Hello World');
+// };
---
+++ HelloWorld_Module/main.js
@@ -0,0 +1,7 @@
+var hello = require('./hello');
+hello.world();
+
+
+//module.exports
+// var hello = require('./hello');
+// var Hello = new hello();
